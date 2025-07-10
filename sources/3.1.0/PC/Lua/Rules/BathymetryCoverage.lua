295ac7e3ce08b9ac1ca2d5b418a7b5b48ae10b0f
Daniel ROHDE <daniel.rohde@bsh.de>
2025-07-10 14:24:52 +0200

    Preparation of S-102 Version 3.1.0

---
+++ sources/3.1.0/PC/Lua/Rules/BathymetryCoverage.lua
@@ -0,0 +1,17 @@
+-- BathymetryCoverage symbology rules file.
+
+-- Main entry point for BathymetryCoverage.
+function BathymetryCoverage(feature, featurePortrayal, contextParameters)
+	featurePortrayal:AddInstructions('ViewingGroup:13030;DrawingPriority:3;DisplayPlane:UnderRadar')
+	
+	featurePortrayal:AddInstructions('CoverageColor:DEPIT,0;LookupEntry:Intertidal,,0,ltSemiInterval')
+	if contextParameters.FourShades then
+		featurePortrayal:AddInstructions('CoverageColor:DEPVS,0;LookupEntry:Shallow Water,0,' .. tostring(contextParameters.ShallowContour) .. ',geLtInterval;CoverageFill:depth')
+		featurePortrayal:AddInstructions('CoverageColor:DEPMS,0;LookupEntry:Medium-Shallow Water,' .. tostring(contextParameters.ShallowContour) .. ',' .. tostring(contextParameters.SafetyContour) .. ',geLtInterval')
+		featurePortrayal:AddInstructions('CoverageColor:DEPMD,0;LookupEntry:Medium-Deep Water,' .. tostring(contextParameters.SafetyContour) .. ',' .. tostring(contextParameters.DeepContour) .. ',geLtInterval')
+		featurePortrayal:AddInstructions('CoverageColor:DEPDW,0;LookupEntry:Deep Water,'.. tostring(contextParameters.DeepContour) .. ',,geSemiInterval')
+	else
+		featurePortrayal:AddInstructions('CoverageColor:DEPVS,0;LookupEntry:Shallow Water,0,' .. tostring(contextParameters.SafetyContour) .. ',geLtInterval;CoverageFill:depth')
+		featurePortrayal:AddInstructions('CoverageColor:DEPDW,0;LookupEntry:Deep Water,'.. tostring(contextParameters.SafetyContour) .. ',,geSemiInterval')
+	end
+end
